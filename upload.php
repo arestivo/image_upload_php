39fff62893c9c68e31fd6a29d37a7259ff7ae80e
André Restivo <andre.restivo@gmail.com>
2023-04-18 13:38:56 +0100

    fix: fixed storing original image if not jpg and ordering of actions

--- upload.php
+++ upload.php
@@ -4,13 +4,6 @@
   $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  // Insert image data into database
-  $stmt = $dbh->prepare("INSERT INTO images VALUES(NULL, ?)");
-  $stmt->execute(array($_POST['title']));
-
-  // Get image ID
-  $id = $dbh->lastInsertId();
-
   // Create folders if they don't exist
 
   if (!is_dir('images')) mkdir('images');
@@ -18,24 +11,39 @@
   if (!is_dir('images/thumbs_small')) mkdir('images/thumbs_small');
   if (!is_dir('images/thumbs_medium')) mkdir('images/thumbs_medium');
 
+  // PHP saves the file temporarily here
+  // Image is the name of the file input in the form
+  $tempFileName = $_FILES['image']['tmp_name'];
+
+  // Create an image representation of the original image
+  // @ before function is to prevent warning messages
+  $original = @imagecreatefromjpeg($tempFileName);
+  if (!$original) $original = @imagecreatefrompng($tempFileName);
+  if (!$original) $original = @imagecreatefromgif($tempFileName);
+
+  if (!$original) die('Unknown image format!');
+
+  // Insert image data into database
+  $stmt = $dbh->prepare("INSERT INTO images VALUES(NULL, ?)");
+  $stmt->execute(array($_POST['title']));
+
+  // Get image ID
+  $id = $dbh->lastInsertId();
+
   // Generate filenames for original, small and medium files
   $originalFileName = "images/originals/$id.jpg";
   $smallFileName = "images/thumbs_small/$id.jpg";
   $mediumFileName = "images/thumbs_medium/$id.jpg";
 
-  // Move the uploaded file to its final destination
-  move_uploaded_file($_FILES['image']['tmp_name'], $originalFileName);
-
-  // Crete an image representation of the original image
-  $original = imagecreatefromjpeg($originalFileName);
-  if (!$original) $original = imagecreatefrompng($originalFileName);
-  if (!$original) $original = imagecreatefromgif($originalFileName);
-
-  if (!$original) die();
-
   $width = imagesx($original);     // width of the original image
   $height = imagesy($original);    // height of the original image
   $square = min($width, $height);  // size length of the maximum square
+
+  // Save original file as jpeg (even if it came in a different format)
+  imagejpeg($original, $originalFileName);
+
+  // We could also copy the file directly without converting to jpeg
+  // move_uploaded_file($_FILES['image']['tmp_name'], $originalFileName);
 
   // Create and save a small square thumbnail
   $small = imagecreatetruecolor(200, 200);
